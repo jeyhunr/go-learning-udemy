9dee7893ac6a2e4a5d44836406f91a0ad2f770f6
Jeyhun Rahimli <j.rahimli@freesixtyfive.de>
2024-12-11 07:38:59 +0100

    add: connect to Postgres DB

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,14 @@
-### backend
+## Backend
+
+### Starting the Database
+To start the database, run the following command from the root level of the project:
+
+```bash
+docker compose up -d
+```
+
+### Starting the Application
+To start the application, use the following command:
+
+```bash
+go run ./cmd/api
--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -12,19 +13,27 @@ const port = 8080
 type application struct {
 	DSN    string
 	Domain string
+	DB     *sql.DB
 }
 
 func main() {
 	var app application
 
+	// read from cmd
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 dbname=movies user=postgres password=postgres sslmode=disable timezone=UTC connect_timeout=5", "PostgreSQL connection string")
 	flag.Parse()
 
+	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+	app.DB = conn
 	app.Domain = "rahimli.net"
 
 	log.Println("Listening on port", port, "with domain:", app.Domain)
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.route()); err != nil {
+	if err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.route()); err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
 }
